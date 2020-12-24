9fc5c851e10d2acd928adb4c1d048c1cce2e8506
mldamico <matias.lioneldamico@gmail.com>
2020-12-24 16:00:22 -0300

    Message post from client to server

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-io.on('connection', (socket) => {
-  socket.emit('mensaje-bienvenida', 'Bienvenido al server');
-  socket.on('mensaje-cliente', (data) => {
-    console.log(data.msg);
+io.on("connection", socket => {
+  socket.emit("mensaje-bienvenida", "Bienvenido al server");
+  socket.on("mensaje-to-server", data => {
+    io.emit("mensaje-from-server", data);
   });
 });
 
 server.listen(3000, () => {
-  console.log('Server corriendo en puerto 3000');
+  console.log("Server corriendo en puerto 3000");
 });
