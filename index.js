c655751485c14b316c60fb53a9482878469a0c5b
mldamico <matias.lioneldamico@gmail.com>
2020-12-23 21:41:45 -0300

    Emit and listen

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const io = require("socket.io")(server);
 
 app.use(express.static(__dirname + "/public"));
 
-io.on("connection", () => {
-  console.log("Cliente conectado");
+io.on("connection", socket => {
+  socket.emit("mensaje-bienvenida", "Bienvenido al server");
 });
 
 server.listen(3000, () => {
-  console.log("Server corriendo en puerto 8080");
+  console.log("Server corriendo en puerto 3000");
 });
